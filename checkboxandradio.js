15776e3aa46793b83c26f8a540be8074ddeb33dc
vaishjm <53874839+vaishjm@users.noreply.github.com>
2024-09-26 13:11:03 +0530

    Add files via upload

---
+++ checkboxandradio.js
@@ -0,0 +1,8 @@
+// Example functions in Gruntfile.js or any JS file
+window.testFunction = function() {
+    console.log('Test function executed from external JS file!');
+};
+
+window.alertFunction = function(message) {
+    alert(message);
+};
